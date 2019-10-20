e66d210f469a70075d00970996674048c0b70c4e
Bryce Mehring <brycemehring@gmail.com>
2019-10-20 12:53:02 -0500

    Set git user

--- deploy.js
+++ deploy.js
@@ -2,6 +2,10 @@ const ghpages = require('gh-pages');
 
 ghpages.publish('dist', {
   branch: 'master',
+  user: {
+    name: 'GitHub Action',
+    email: 'action@github.com'
+  }
 }, (err) => {
   if (err) {
     console.error(err);
