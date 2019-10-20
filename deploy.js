ac2f39fd79a3ab85b3332c3d13294748c0392f2c
Bryce Mehring <brycemehring@gmail.com>
2019-10-20 13:12:03 -0500

    Set repo with GITHUB_TOKEN

--- deploy.js
+++ deploy.js
@@ -1,13 +1,19 @@
 const ghpages = require('gh-pages');
+const {
+  GITHUB_ACTOR,
+  GITHUB_REPOSITORY,
+  GITHUB_TOKEN,
+} = process.env;
 
 ghpages.publish('dist', {
   branch: 'master',
+  repo: `https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git`,
   user: {
-    name: 'GitHub Action',
+    name: GITHUB_ACTOR,
     email: 'action@github.com'
   }
 }, (err) => {
   if (err) {
-    console.error(err);
+    console.error('\n' + err);
   }
 });
