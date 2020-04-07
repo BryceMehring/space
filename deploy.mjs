9badf5dda3e4afc6445a4295628162e967ab98e1
Bryce Mehring <brycemehring@gmail.com>
2020-04-06 19:49:30 -0500

    Use common deploy script

--- deploy.mjs
+++ deploy.mjs
@@ -1,24 +1,3 @@
-import ghpages from 'gh-pages';
-import { spawnSync } from 'child_process';
+import { deploy } from '@brycemehring/gh-pages-deploy';
 
-const gitShowCommand = 'show -s --format=reference';
-
-const output = spawnSync('git', gitShowCommand.split(' '));
-
-if (output.status === 0) {
-  const message = `Built from ${output.stdout.toString()}`;
-
-  ghpages.publish('dist', {
-    branch: 'master',
-    history: false,
-    message,
-  }, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('Deploy successful');
-    }
-  });
-} else {
-  console.error(output.stderr.toString());
-}
+deploy('dist', 'master');
