f9f9d131080f44b33852848767f06bce464c5bf1
Bryce Mehring <brycemehring@gmail.com>
2026-03-20 22:13:15 -0500

    upgrade packages upgrade deploy script

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import { resolve } from 'node:path';
 export default defineConfig({
   build: {
     modulePreload: false,
-    minify: 'terser',
-    target: 'esnext',
   },
   worker: {
     format: 'es',
